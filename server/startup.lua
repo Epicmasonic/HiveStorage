1b58d6d1ed3070ee1f6f6dc64186b2adbf2abe67
Epicmasonic <epicmasonic@gmail.com>
2026-08-28 16:45:11 -0400

    Clean up a bit

--- server/startup.lua
+++
@@ -1,4 +0,0 @@
----@diagnostic disable: undefined-global, undefined-field, trailing-space
-
-local monitor = peripheral.find("monitor")
-shell.run("monitor",peripheral.getName(monitor),"hiveStorage/server.lua")
